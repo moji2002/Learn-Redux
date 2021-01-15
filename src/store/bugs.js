4feb7c2a905c1700dabdfccbe64cb7a72c1e7b33
Mojtaba <moji2002@gmail.com>
2021-01-15 12:46:41 +0330

    createSlice function

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,36 +1,37 @@
-import { createAction, createReducer } from '@reduxjs/toolkit'
+import { createAction, createReducer, createSlice } from '@reduxjs/toolkit'
 
+let lastId = 1000;
+/*
 // Action creaters with redux toolkit
 export const bugRemoved = createAction('bugRemoved')
 export const bugAdded = createAction('bugAdded')
 export const bugResolved = createAction('bugResolved')
+*/
 
-let lastId = 1000;
-
-
-// const reducer = (state = [], action) => {
-//   switch (action.type) {
-//     case bugAdded.type:
-//       return [
-//         ...state,
-//         {
-//           id: ++lastId,
-//           description: action.payload.description,
-//           resolved: false,
-//         },
-//       ];
-//     case bugRemoved.type:
-//       return state.filter((bug) => bug.id !== action.payload.id);
-//     case bugResolved.type:
-//       return state.map((bug) =>
-//         bug.id !== action.payload.id ? bug : { ...bug, resolved: true }
-//       );
-//     default:
-//       return state;
-//   }
-// };
-
-
+/*
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case bugAdded.type:
+      return [
+        ...state,
+        {
+          id: ++lastId,
+          description: action.payload.description,
+          resolved: false,
+        },
+      ];
+    case bugRemoved.type:
+      return state.filter((bug) => bug.id !== action.payload.id);
+    case bugResolved.type:
+      return state.map((bug) =>
+        bug.id !== action.payload.id ? bug : { ...bug, resolved: true }
+      );
+    default:
+      return state;
+  }
+};
+*/
+/*
 // Using createReducer function
 // this function uses immer under the hood
 // if we use redux toolkit we dont have to worry about default case
@@ -50,5 +51,26 @@ const reducer = createReducer([], {
     bugs[index].resolved = true;
   },
 })
+*/
+// by using createSlice function we can combine createAction and createReducer 
 
+const reducer = createSlice({
+  name: "bugs",
+  initialState: [],
+  reducers: {
+    bugAdded: (bugs, action) => {
+      bugs.push({
+        id: ++lastId,
+        description: action.payload.description,
+        resolved: false
+      })
+    },
+    bugResolved: (bugs, action) => {
+      const index = bugs.findIndex(bug => bug.id === action.payload.id);
+      bugs[index].resolved = true;
+    }
+  }
+})
+
+export const { bugAdded, bugResolved } = reducer.actions;
 export default reducer;
