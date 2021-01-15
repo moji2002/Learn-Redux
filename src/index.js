21666c90ce8ea5e3242d7dce651fba155b2b2479
Mojtaba <moji2002@gmail.com>
2021-01-15 12:14:51 +0330

    createReducer function

--- src/index.js
+++ src/index.js
@@ -2,18 +2,17 @@ import store from './store/store'
 import { bugAdded, bugResolved } from './store/bugs'
 // console.log(store);
 
-// const unsubscribe = store.subscribe(() => {
-//   // this function get called every time store changed
-//   console.log("store changed", store.getState());
-// });
+const unsubscribe = store.subscribe(() => {
+  // this function get called every time store changed
+  console.log("store changed", store.getState());
+});
 
 // subscribe will return the unsubscribe function
-
-// unsubscribe();
 
 store.dispatch(bugAdded({ description: "new bug 1" }));
 store.dispatch(bugAdded({ description: "new bug 2" }));
 store.dispatch(bugAdded({ description: "new bug 3" }));
+unsubscribe();
 store.dispatch(bugAdded({ description: "new bug 4" }));
 store.dispatch(bugResolved({ id: 1002 }));
 
--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,4 +1,4 @@
-import { createAction } from '@reduxjs/toolkit'
+import { createAction, createReducer } from '@reduxjs/toolkit'
 
 // Action creaters with redux toolkit
 export const bugRemoved = createAction('bugRemoved')
@@ -6,26 +6,49 @@ export const bugAdded = createAction('bugAdded')
 export const bugResolved = createAction('bugResolved')
 
 let lastId = 1000;
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case bugAdded.type:
-      return [
-        ...state,
-        {
-          id: ++lastId,
-          description: action.payload.description,
-          resolved: false,
-        },
-      ];
-    case bugRemoved.type:
-      return state.filter((bug) => bug.id !== action.payload.id);
-    case bugResolved.type:
-      return state.map((bug) =>
-        bug.id !== action.payload.id ? bug : { ...bug, resolved: true }
-      );
-    default:
-      return state;
-  }
-};
+
+
+// const reducer = (state = [], action) => {
+//   switch (action.type) {
+//     case bugAdded.type:
+//       return [
+//         ...state,
+//         {
+//           id: ++lastId,
+//           description: action.payload.description,
+//           resolved: false,
+//         },
+//       ];
+//     case bugRemoved.type:
+//       return state.filter((bug) => bug.id !== action.payload.id);
+//     case bugResolved.type:
+//       return state.map((bug) =>
+//         bug.id !== action.payload.id ? bug : { ...bug, resolved: true }
+//       );
+//     default:
+//       return state;
+//   }
+// };
+
+
+// Using createReducer function
+// this function uses immer under the hood
+// if we use redux toolkit we dont have to worry about default case
+const reducer = createReducer([], {
+  // key: value
+  // actions: function (event => event handler)
+  [bugAdded.type]: (bugs, action) => {
+    bugs.push({
+      id: ++lastId,
+      description: action.payload.description,
+      resolved: false
+    })
+  },
+
+  [bugResolved.type]: (bugs, action) => {
+    const index = bugs.findIndex(bug => bug.id === action.payload.id);
+    bugs[index].resolved = true;
+  },
+})
 
 export default reducer;
